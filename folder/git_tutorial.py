b50b97fc8b10937157b17cf954bd911793affda2
Siraj Salim <sirajsalim@gmail.com>
2024-12-16 21:24:24 +0000

    added a file and testing commit

--- folder/git_tutorial.py
+++ folder/git_tutorial.py
@@ -1,6 +1,12 @@
-def add_number(a, b, c):
+def add_number(a, b, c) -> int:
     """ Return the sum of a, b and c """
     return sum([a, b, c])
 
-def calc_difference(a, b):
+
+
+def calc_difference(a, b) -> str:
     return a - b
+
+result:str = calc_difference(2,3)
+print (type(result))
+
---
+++ test_pylance.py
@@ -0,0 +1,9 @@
+def add_numbers(a: int, b: int) -> int:
+    return a + b
+
+
+# number_sum: int  = add_numbers("5", 3)
+
+number_sum2 = add_numbers("5",4)
+print (number_sum2)
+
